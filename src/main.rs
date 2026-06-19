61b4c276838148d90c3c34bc96a7758b170efa34
Arthur Tesliuk <arthurtesliuk@gmail.com>
2026-06-19 16:07:40 -0300

    tudo pronto e funcionando, novas dependencias adicionadas

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 use std::io;
+use rpassword::read_password;
 
-fn main(){
-    let users = ["admin"];      // users array
-    //
-    let passwd = ["admin 123"]; // passwords array
-    //
-    let mut login = String::new(); // input for the login
-    //
-    io::stdin().read_line(&mut login).expect("unexpected error");
-    //
-    let mut password = String::new(); // input for the password
-    //
-    io::stdin().read_line(&mut password).expect("unexpected error");
+use std::{thread, time::Duration, io::{Write}};
 
+fn write(text: &str, char_delay_ms: u64) {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    for c in text.chars() {
+        print!("{}", c);
+        handle.flush().unwrap();
+        thread::sleep(Duration::from_millis(char_delay_ms));
+    }
+}
 
+fn input(mut vari: &mut String){
+    io::stdin().read_line(&mut vari).expect("unexpected error");
+
+}
+
+fn main() -> std::io::Result<()> {
+    let users = ["admin"];
+    let passwd = ["admin123"];
+
+    let mut login = String::new();
+    write("qual seu login?:\n", 50);
+    input(&mut login);
+    let login = login.trim();
+
+    write("qual sua senha?:\n", 50);
+    let password = read_password()?;
+    let password = password.trim(); 
+
+    if passwd.contains(&password) && users.contains(&login) {
+        write("acertou a senha e o usuario", 50);
+    } else {
+        write("usuario ou senha incorretos", 50);
+    }
+    Ok(())
 }
